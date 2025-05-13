0c2b3fcd1ed6a5a69faf7b1e2b01fa4b719c575d
Kon James Ayuen <nyiel37@gmail.com>
2025-05-13 19:40:48 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template # type: ignore
+import joblib # type: ignore
+
+app = Flask(__name__)
+
+# Load model and vectorizer
+model = joblib.load('spam_rf_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']
+    data = vectorizer.transform([message])
+    prediction = model.predict(data)[0]
+    label = 'Spam' if prediction == 1 else 'Ham'
+    return render_template('index.html', prediction_text=f'Prediction: {label}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
